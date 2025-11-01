ff4073f0aa7b126f19a317b4f8dd50081db8e97f
M-634 <miyanagateru634@gmail.com>
2025-11-01 20:43:14 +0900

    add migration directory

---
+++ pj-server/Api/Migrations/20251101073136_AddMasterTables.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMasterTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "MItems",
+                columns: table => new
+                {
+                    item_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    item_type = table.Column<byte>(type: "tinyint unsigned", nullable: false),
+                    max_count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MItems", x => x.item_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "MItemLocalizes",
+                columns: table => new
+                {
+                    item_id = table.Column<int>(type: "int", nullable: false),
+                    text = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MItemLocalizes", x => x.item_id);
+                    table.ForeignKey(
+                        name: "FK_MItemLocalizes_MItems_item_id",
+                        column: x => x.item_id,
+                        principalTable: "MItems",
+                        principalColumn: "item_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MItemLocalizes");
+
+            migrationBuilder.DropTable(
+                name: "MItems");
+        }
+    }
+}
