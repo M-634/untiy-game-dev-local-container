5b3499544f90d1c3a0c8ea8ff7ca2516a3bb1a4e
M-634 <miyanagateru634@gmail.com>
2025-11-01 20:41:14 +0900

    create cofing.cs

--- pj-server/Api/Domain/Common/Config.cs
+++ pj-server/Api/Domain/Common/Config.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Domain.Common;
 
-public class Config
+public static class Config
 {
-    
+    public const int MaxLength64Letters = 64;
+    public const int MaxLength128Letters = 128;
+    public static readonly MySqlServerVersion MySqlServerVersion = new(new Version(8, 4, 0));
 }
