c4ed99e9703af1864e33a7827c367a9aea820a49
M-634 <miyanagateru634@gmail.com>
2025-11-29 11:29:24 +0900

    remove MasterAtrrbute.cs

--- pj-master/schemas/MasterAttribute.cs
+++
@@ -1,8 +0,0 @@
-// namespace pj_master.Schemas;
-//
-// [AttributeUsage(AttributeTargets.Class)]
-// public class MasterAttribute(string masterName) : Attribute
-// {
-//     public string MasterName { get; } = masterName;
-// }
-
